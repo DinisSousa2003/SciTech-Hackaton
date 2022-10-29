416a640eb8af9989544c1d9defb16518dcfcfb64
DinisSousa2003 <dinis.bessa.sousa@gmail.com>
2022-10-29 16:40:11 +0100

    Add semicolon

--- festivus/lib/view/Pages/admin.dart
+++ festivus/lib/view/Pages/admin.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class AdminPage extends StatelessWidget {
   const AdminPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,13 +11,71 @@ class AdminPage extends StatelessWidget {
         title: Text('Admin'),
       ),
       body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go Back'),
+        child: FormWidget()
+        ),
+      );
+  }
+}
+
+class FormWidget extends StatefulWidget {
+  const FormWidget({Key? key}) : super(key: key);
+
+  @override
+  State<FormWidget> createState() => _FormWidgetState();
+}
+
+class _FormWidgetState extends State<FormWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[ Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            formField('name'),
+            formField('place'),
+            formField('start time'),
+            formField('end time'),
+            formField('date (dd-mm-yy)'),
+            formField('description'),
+            formField('photo (link)'),
+            formField('price (€)')
+          ],
         ),
       ),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: ElevatedButton(
+          onPressed: () {
+            // Validate will return true if the form is valid, or false if
+            // the form is invalid.
+            if (_formKey.currentState!.validate()) {
+              // Process data.
+            }
+          },
+          child: const Text('Submit'),
+        ),
+      ),
+    ]);
+  }
+
+  Widget formField(String text){
+    return TextFormField(
+          decoration: InputDecoration(
+            hintText: 'Your event ' + text,
+          ),
+          validator: (String? value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter some text';
+            }
+            return null;
+          }
     );
   }
 }
+
+
+
