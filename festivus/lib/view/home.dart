137b53cad885b99b7e5f60b12f403779a17486e5
DinisSousa2003 <dinis.bessa.sousa@gmail.com>
2022-10-29 13:57:27 +0100

    Started proj

---
+++ festivus/lib/view/home.dart
@@ -0,0 +1,26 @@
+import 'package:festivus/view/party.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return const SecondPage(title: 'SecondPage');
+            }));
+          },
+          child: const Text('Next'),
+        ),
+      ),
+    );
+  }
+}
